f66ad57071b961c3474223f26fd9bb624a3a29ea
qkrcksgk02 <qkrcksgk02@korea.ac.kr>
2024-03-29 01:30:14 +0900

    Commit #3 : Add send-arp_func.cpp

---
+++ _send-arp_func.cpp
@@ -0,0 +1,16 @@
+#include "send-arp.h"
+
+int		_get_my_ip(char **ip)
+{
+	return (0);
+}
+
+int		_get_my_mac(char **mac)
+{
+	return (0);
+}
+
+int		_get_victim_mac(char **mac)
+{
+	return (0);
+}
---
+++ send-arp.h
@@ -0,0 +1,12 @@
+#ifndef SEND_ARP_H
+
+#define SEND_ARP_H
+
+#include <stdio.h>
+
+/*		_send-arp_func.cpp		*/
+int		_get_my_ip(char **ip);
+int		_get_my_mac(char **mac);
+int		_get_victim_mac(char **mac);
+
+#endif
